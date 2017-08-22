3f3b208dd3dc6cbdb99eee31246d97e3a8acf73c
brandenbyers <branden@brandenbyers.com>
2017-08-21 20:15:39 -0700

    Fix the error in indentation

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 var temp = document.querySelector('.time');
-var button = document.querySelector("button"); var words = document.querySelector(".words"); var timerDiv = document.querySelector(".time"); var scoreDiv = document.querySelector(".score"); var points = 0; var spans; var typed; var seconds = 30; var spark = new Audio(""); function countdown() { points = 0; var timer = setInterval(function(){ button.disabled = true; seconds--; temp.innerHTML = seconds; if (seconds === 0) { console.log("Game over! Your score is " + points); words.innerHTML = ""; button.disabled = false; clearInterval(timer); seconds = 30; timerDiv.innerHTML = "30"; button.disabled = false;	} }, 1000); } function random() { words.innerHTML = ""; var random = Math.floor(Math.random() * (1943 - 0 + 1)) + 0; var wordArray = list[random].split(""); for (var i = 0; i < wordArray.length; i++) { var span = document.createElement("span"); span.classList.add("span"); span.innerHTML = wordArray[i]; words.appendChild(span);
+var button = document.querySelector("button");
+var words = document.querySelector(".words");
+var timerDiv = document.querySelector(".time");
+var scoreDiv = document.querySelector(".score");
+var points = 0;
+var spans;
+var typed;
+var seconds = 30;
+var spark = new Audio("");
+
+function countdown() {
+  points = 0;
+  var timer = setInterval(function(){
+    button.disabled = true;
+      seconds--;
+      temp.innerHTML = seconds;
+      if (seconds === 0) {
+        console.log("Game over! Your score is " + points);
+        words.innerHTML = "";
+        button.disabled = false;
+        clearInterval(timer);
+        seconds = 30;
+        timerDiv.innerHTML = "30";
+        button.disabled = false;
+      }
+  }, 1000);
+  }
+
+  function random() {
+    words.innerHTML = "";
+    var random = Math.floor(Math.random() * (1943 - 0 + 1)) + 0;
+    var wordArray = list[random].split("");
+    for (var i = 0; i < wordArray.length; i++) {
+      var span = document.createElement("span");
+      span.classList.add("span");
+      span.innerHTML = wordArray[i];
+      words.appendChild(span);
     }
     spans = document.querySelectorAll(".span");
   }
